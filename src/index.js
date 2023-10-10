6d867de2d1f9ef916282d5a5522641ce5b7b77bd
DangMinh <dangminh1312@gmail.com>
2023-10-10 21:29:21 +0700

    create simple form which contains addBook and deleteBook

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import { initializeApp } from 'firebase/app'
 import { 
     getFirestore,
     collection,
-    getDocs
-
+    getDocs,
+    addDoc,
+    deleteDoc,
+    doc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -37,3 +39,28 @@ getDocs(colRef)
     .catch((err) => {
         console.log(err)
     })
+
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', e => {
+    e.preventDefault()
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        value: addBookForm.author.value,
+    })
+    .then(() => {
+        addBookForm.reset()
+    })
+
+})
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+    deleteDoc(docRef)
+        .then(() => {
+            deleteBookForm.reset()
+        })
+})
